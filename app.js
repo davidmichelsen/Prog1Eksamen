4b39a5b6e46eaf5085e1a0879b1e65fd46602ad8
davidmichelsen <dmtiger10@gmail.com>
2020-10-22 12:32:23 +0200

    tilføjet ejs, multer, cors

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 
+//Initialise application
 const app = express();
 
+//Initialize database connection
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true });
 
+//Check connection to database
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
@@ -13,6 +16,7 @@ db.once('open', function() {
 });
 
 
+//Start application
 app.listen(3000, () => {
 
 console.log("Server listening on port 3000");
